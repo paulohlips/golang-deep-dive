3d072f45aa4ee55cc16bd930bcf6b50c66b29c7d
paulohlips <paulohfcmendes@gmail.com>
2023-10-29 15:47:52 -0300

    feat: using buffer to read chunks/bytes of file

--- 01-important-packets/01-file-handling.go
+++ 01-important-packets/01-file-handling.go
@@ -1,27 +1,52 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
-	f, err := os.Create("newfile.txt")
+	fileName := "new-file.txt"
+	f, err := os.Create(fileName)
 
 	if err != nil {
 		panic(err)
 	}
 
-	length, err := f.Write([]byte("Creating new file \n"))
+	length, err := f.Write([]byte("Creating new file big file to print using buffer\n"))
+
+	if err != nil {
+		panic(err)
+	}
 
 	fmt.Printf("File created! length: %d bytes", length)
 
 	f.Close()
 
-	file, err := os.ReadFile("newfile.txt")
+	file, err := os.ReadFile(fileName)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Print(string(file))
+
+	// using buffers"newFile.txt"
+	bigFile, err := os.Open(fileName)
+
+	if err != nil {
+		panic(err)
+	}
+
+	reader := bufio.NewReader(bigFile)
+	buffer := make([]byte, 1)
+
+	for {
+		n, err := reader.Read(buffer)
+		if err != nil {
+			break
+		}
+
+		fmt.Println(string(buffer[:n]))
+	}
 }
