cce6e22c7b8b156df8a8cbbbabe31f3cf013c56f
paulohlips <paulohfcmendes@gmail.com>
2023-10-31 15:03:24 -0300

    feat: get query param

--- 06-http-server/main.go
+++ 06-http-server/main.go
@@ -3,10 +3,21 @@ package main
 import "net/http"
 
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", FindCep)
 	http.ListenAndServe(":8080", nil)
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello!"))
+func FindCep(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	cep := r.URL.Query().Get("cep")
+	if cep == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("CEP"))
 }
