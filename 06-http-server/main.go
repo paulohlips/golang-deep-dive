7cc6f1f3525da35c4a4da10dbe006ce636f6c84c
paulohlips <paulohfcmendes@gmail.com>
2023-11-01 08:32:53 -0300

    feat: adds func to get cep

--- 06-http-server/main.go
+++ 06-http-server/main.go
@@ -1,6 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type CepResponse struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+}
 
 func main() {
 	http.HandleFunc("/", FindCep)
@@ -17,7 +32,31 @@ func FindCep(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	cepResponse, err := GetCep(cep)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	w.Header().Set("Content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("CEP"))
+
+	json.NewEncoder(w).Encode(cepResponse)
+}
+
+func GetCep(cep string) (*CepResponse, error) {
+	response, err := http.Get("https://viacep.com.br/ws/" + cep + "/json")
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	var result CepResponse
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
 }
